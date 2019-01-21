c5d2a0ceb90ff317e98ad3f2d386ffffe18e8d4c
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-01-21 12:18:48 +0000

    Added missing collate

--- com_userinvites/admin/sql/install.mysql.utf8.sql
+++ com_userinvites/admin/sql/install.mysql.utf8.sql
@@ -5,7 +5,7 @@ CREATE TABLE `#__userinvites` (
   `email` VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL,
   `code` VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL,
   `groups` TEXT COLLATE utf8_unicode_ci,
-  `email_body` TEXT NOT NULL,
+  `email_body` TEXT COLLATE utf8_unicode_ci NOT NULL,
   `sent_by` INT(10) UNSIGNED NOT NULL DEFAULT '0',
   `sent_on` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
   `expires` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
